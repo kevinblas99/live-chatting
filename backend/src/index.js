330bc5e8c55705af4906a6b933a199c0d9e63017
kevinblas99 <116315967+kevinblas99@users.noreply.github.com>
2025-02-26 15:48:54 -0500

    socket implemention

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,20 +5,25 @@ import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
+import cors from "cors"
+import { app, server } from './lib/socket.js';
 
 dotenv.config()
 
-const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+})),
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(5001, () => {
+server.listen(5001, () => {
     console.log("Server is running on PORT:" + PORT);
     connectDB()
 });
